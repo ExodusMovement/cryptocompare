f7c1b4909142541aabd20acc5f7c8666d4e4f6b5
JP Richardson <jprichardson@gmail.com>
2016-08-01 16:03:27 -0500

    test: rename

--- test/cryptocompare.test.js
+++ test/cryptocompare.test.js
@@ -7,6 +7,7 @@ var cc = require('../')
 test('coinList()', function (t) {
   t.plan(1)
   cc.coinList().then(coinList => {
+    console.dir(coinList.BTC)
     t.strictEqual(coinList.BTC.CoinName, 'Bitcoin')
   }).catch(t.end)
 })
